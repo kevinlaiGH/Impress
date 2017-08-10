7b5f1e7455aaa89c8470224509aa7930b5c2602d
Young Jae Nam <Jaenam@Young-Jae-Nams-MacBook-Air.local>
2017-08-10 13:25:45 +1000

    first prqject q Pleasq wnter thq commiq message for your changes. Lines starting

---
+++ Project1.rb
@@ -0,0 +1,48 @@
+#I want to buy this item(s) that costs $____ by this date how much money do i need to save each week to obtain this item(s).
+# Note: It can be converted to hours (how much hours I need to work to get this item)-----> a bar graph of hours
+
+class User
+  def initialize(name, age, balance)
+    @name = name
+    @age = age
+    @balance = balance
+  end
+  attr_accessor :name, :age, :balance
+  # def
+  #   savings = (student.balance -)
+  # end
+end
+
+class Time
+  def initialize(amount)
+    @amount = amount
+  end
+  attr_accessor :amount
+end
+
+class Expense
+  def initialize(spending)
+    @spending = spending
+  end
+  attr_accessor :spending
+end
+
+
+array = []
+puts "Greeting. Enter Name:"
+name = gets.chomp
+puts "Hello #{name}, How old are you?"
+age = gets.chomp.to_i
+puts "Please state your monthly income"
+balance = gets.chomp.to_i
+student = User.new(name, age, balance)
+puts "So your current balane is #{student.balance}"
+
+puts "Enter expenses:"
+array << gets.chomp
+puts "Enter 2nd"
+array << gets.chomp
+
+money = Expense.new(array)
+
+puts money.spending
